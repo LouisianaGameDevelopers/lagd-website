d3a6a5f74dedf9d35f8e5dd58933056bb9d6cccc
Nicholas Scheurich <nick@scheurich.me>
2018-03-06 10:46:54 -0600

    Update title and description; remove unneeded code; formatting

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,35 +1,27 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import Helmet from 'react-helmet'
+import React from "react";
+import PropTypes from "prop-types";
+import Helmet from "react-helmet";
 
-import Header from '../components/Header'
-import './index.css'
+import "./index.css";
+
+const title = "Louisiana Game Developers";
+const description = `
+Louisiana Game Developers is an organization dedicated to fostering
+and promoting the game development community in Louisiana.
+`;
 
 const TemplateWrapper = ({ children }) => (
   <div>
     <Helmet
-      title="Gatsby Default Starter"
-      meta={[
-        { name: 'description', content: 'Sample' },
-        { name: 'keywords', content: 'sample, something' },
-      ]}
+      title={title}
+      meta={[{ name: "description", content: description }]}
     />
-    <Header />
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
-      }}
-    >
-      {children()}
-    </div>
+    <div>{children()}</div>
   </div>
-)
+);
 
 TemplateWrapper.propTypes = {
-  children: PropTypes.func,
-}
+  children: PropTypes.func
+};
 
-export default TemplateWrapper
+export default TemplateWrapper;
