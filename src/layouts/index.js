edda3e18e054ac095a305627b30c224b934edf9b
Nicholas Scheurich <nick@scheurich.me>
2018-03-20 15:03:33 -0500

    Update OG tags

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -3,19 +3,28 @@ import PropTypes from "prop-types";
 import Helmet from "react-helmet";
 
 import favicon from "../static/favicon.ico";
+import { batonRouge } from "../static/images";
 import "./index.css";
 
 const title = "Louisiana Game Developers";
 const description = `
 Louisiana Game Developers is an organization dedicated to fostering
-and promoting the game development community in Louisiana.
+and promoting the game development community in Louisiana. [Image (modified)
+is CC BY-SA 2.0 by neil o (http://bit.ly/2FWcani)]
 `;
 
 const TemplateWrapper = ({ children }) => (
   <div>
     <Helmet
       title={title}
-      meta={[{ name: "description", content: description }]}
+      meta={[
+        { name: "description", content: description },
+        { name: "og:url", content: "https://www.lagd.network/" },
+        { name: "og:type", content: "website" },
+        { name: "og:title", content: "Louisiana Game Developers" },
+        { name: "og:image", content: batonRouge },
+        { name: "og:description", content: description }
+      ]}
       link={[{ rel: "icon", href: favicon }]}
     />
     <div>{children()}</div>
