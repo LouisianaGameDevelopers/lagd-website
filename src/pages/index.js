f2d428912f7abab44c2c59fb21383745607e3423
Nicholas Scheurich <nick@scheurich.me>
2018-04-07 15:06:23 -0500

    Add directory buttons

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,6 +8,11 @@ import Discord from "../components/Discord";
 
 import { logo } from "../static/images";
 
+const developerForm =
+  "https://docs.google.com/forms/d/13h_t30lTZ_KYVJ7B8DcPUg88gU7Uv48KXmhpMHWq4jQ";
+const gameForm =
+  "https://docs.google.com/forms/u/2/d/13svBBlZCaI5d4X7O0J3G6cYKHd9TV-OKFlmLVPFUMiI";
+
 class IndexPage extends Component {
   constructor(props) {
     super(props);
@@ -50,6 +55,59 @@ class IndexPage extends Component {
           </div>
         </div>
 
+        <div id="directory" className="bg-white text-black px-6">
+          <div className="container md:flex justify-between items-center mx-auto py-8 md:py-xl">
+            <div className="md:w-1/2">
+              <section className="w-full max-w-sm">
+                <h1 className="leading-tight text-xl text-normal mb-2 uppercase">
+                  LAGD Directory
+                </h1>
+
+                <div className="leading-normal">
+                  <p className="mb-4">
+                    We’re working on compiling an online directory of
+                    Louisiana-based game developers and the games y’all have
+                    created—and we need your help! If you’re a{" "}
+                    <strong>developer</strong> and you’d like to be included in
+                    the directory, create a{" "}
+                    <a
+                      href={developerForm}
+                      className="text-sunset no-underline hover:fade"
+                    >
+                      Developer Submission
+                    </a>. If you’re a developer and have created one or more{" "}
+                    <strong>games</strong>, fill out a{" "}
+                    <a
+                      href={gameForm}
+                      className="text-teal no-underline hover:fade"
+                    >
+                      Game Submission
+                    </a>{" "}
+                    for each one!
+                  </p>
+                </div>
+              </section>
+            </div>
+
+            <div className="md:w-1/2">
+              <a
+                href={developerForm}
+                className="mb-4 block w-full md:w-1/2 px-6 py-4 bg-sunset text-white rounded no-underline mx-auto text-center font-semibold uppercase text-sm shadow-md hover:fade"
+              >
+                Developer Submission
+              </a>
+
+              <a
+                href={gameForm}
+                className="bg-teal block w-full md:w-1/2 px-6 py-4 bg-black text-white rounded no-underline mx-auto text-center font-semibold uppercase text-sm shadow-md hover:fade"
+                style={{ backgroundColor: "#23abae" }}
+              >
+                Game Submission
+              </a>
+            </div>
+          </div>
+        </div>
+
         <div className="relative">
           <EventCard left={this.state.eventCardLeft} />
           <EventMap
@@ -63,7 +121,7 @@ class IndexPage extends Component {
         <div className="bg-black container mx-auto px-6 py-8">
           <div className="md:flex justify-between items-center">
             <a
-              className="text-white no-underline hover:fade block mb-4 md:mb-0"
+              className="text-teal no-underline hover:fade block mb-4 md:mb-0"
               href="mailto:hello@lagd.network"
             >
               hello@lagd.network
